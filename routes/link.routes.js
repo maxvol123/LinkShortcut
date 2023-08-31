fdfaaf163be5126ccf592e8a63945f0a029c2b46
Maksym Voloshyn <maxvol200727@gmail.com>
2023-08-31 14:18:20 +0300

    Fix bug with create link

--- routes/link.routes.js
+++ routes/link.routes.js
@@ -8,21 +8,17 @@ routerl.post("/add",
 async (req, res) => {
     try {
         const baseUrl = config.get("baseUrl")
-        const {from} = req.body.from
+        const from = req.body.from
         const code = shortid.generate()
-        const existing = await Link.findOne({from})
-        if (existing) {
-            return res.json({
-                link:existing
-            })
-        }
         const to = baseUrl+"/t/"+code
         const token = req.body.headers.authorization
         const decodedToken = jwt.verify(token, config.get("jwtKey"))
+        console.log(from);
         const link = new Link({
             code, to, from, owner: decodedToken.userId
         })
         await link.save()
+        console.log(link);
         res.status(201).json({link})
 }catch (err) {
     console.error('Error create link:', err);
